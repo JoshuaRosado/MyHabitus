e9aee6765ec5e12988855e1d141eee49dad17c40
JoshuaRosado <joshua06rosado@gmail.com>
2026-02-28 20:19:11 -0600

    Initial Commit

---
+++ MyHabitus/MyHabitusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitusApp.swift
+//  MyHabitus
+//
+//  Created by Joshua Rosado Olivencia on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHabitusTests/MyHabitusTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyHabitusTests.swift
+//  MyHabitusTests
+//
+//  Created by Joshua Rosado Olivencia on 2/28/26.
+//
+
+import Testing
+
+struct MyHabitusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
